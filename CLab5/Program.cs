f16611f32c82ddde6c775e9fb3b4519450e9964b
dapastushenko <32649673+dapastushenko@users.noreply.github.com>
2019-11-13 11:20:14 +0300

    Done Lab 5

--- CLab5/Program.cs
+++ CLab5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,64 @@ namespace CLab5
     {
         static void Main(string[] args)
         {
-            ;
+            Console.WriteLine(args.Length);
+            //foreach(string arg in args)
+            //{
+            //    Console.WriteLine(arg);
+            //}
+            try{
+                String filenameWDir = args[0];
+                FileStream fileStream = new FileStream(filenameWDir, FileMode.Open);
+                StreamReader streamReader = new StreamReader(fileStream);
+                int size = (int)fileStream.Length;
+                char[] contents = new char[size];
+                checked{
+                    for(int i = 0; i < size; i++)
+                    {
+                        //зачем здесь каст?
+                        contents[i] = (char)streamReader.Read();
+                    }
+                }
+                foreach(char ch in contents)
+                {
+                    Console.WriteLine(ch);
+                }
+                Summarize(contents);
+            }
+            catch(Exception e){
+                Console.WriteLine(e);
+            }
+
+            //just waiting key, comment in prod
+            Console.ReadKey();
+        }
+
+        static void Summarize(char[] charArr)
+        {
+            int numConsonants = 0;
+            int numVowels = 0;
+            int numLines = 0;
+
+            String allСonsonants = "bcdfghjklmnpqrstvwxz";
+            //String allVowels = "аеiouy";
+            foreach (char ch in charArr)
+            {
+                if (Char.IsLetter(ch))
+                {
+                    if (allСonsonants.IndexOf(Char.ToLower(ch)) != -1)
+                    {
+                        numConsonants++;
+                    }
+                    else
+                        numVowels++;
+                }else if (ch == '\n') { numLines++; }
+            }
+
+            Console.WriteLine("Total no of characters: {0}", charArr.Length);
+            Console.WriteLine("Total no of vowels : {0}", numVowels);
+            Console.WriteLine("Total no of consonants: {0}", numConsonants);
+            Console.WriteLine("Total no of lines : {0}", numLines);
+
         }
     }
 }
